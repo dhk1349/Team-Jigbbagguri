ddb8a0690a9ad1b041b319cbdeb12862325c0bef
dhk1349 <41980618+dhk1349@users.noreply.github.com>
2019-09-12 00:55:23 +0900

    .

--- PE_parser/modules/NT_Header.py
+++ PE_parser/modules/NT_Header.py
@@ -23,7 +23,7 @@ def find(fd, NT_Offset):
     SizeOfOptionalHeader = libs.little(data[20:22])
     Chars = libs.little(data[22:24])
     
-    print("[NT_Header]")
+    print("[NT_Header]-File Header")
     print("{}\t\t{}\t\t{} ".format("offset","value","description"))
     print("============================================")
     print("{:08x}\t{:04x}\t{}".format(NT_Offset+4, Machine, "Machine"))
@@ -31,15 +31,19 @@ def find(fd, NT_Offset):
     print("{:08x}\t{:04x}\t{}".format(NT_Offset+8, TimeDateStamp, "TimeDateStamp"))
     print("{:08x}\t{:04x}\t{}".format(NT_Offset+20, SizeOfOptionalHeader, "SizeOfOptionalHeader"))
     print("{:08x}\t{:04x}\t{}".format(NT_Offset+28, Chars, "Characteristics"))
+    print("\n")
     
 #This part is Optional Header 
 #This part is not tested
-    fd.seek(NT_Offset+20, 0)
+    fd.seek(NT_Offset+24, 0)
     data=fd.read(224)  #need to check exact number
     Magic=libs.little(data[0:2]) #010B or 020B
-    BitStructure = "32bits" #determine if its 32 bits or 64 bits
-    if(Magic==0x020B):
-        BitStructure="64bits"
+    BitStructure = "NONE"
+    if (Magic==0x010B):
+        BitStructure = "32bits" #determine if its 32 bits or 64 bits
+    elif(Magic==0x020B):
+        BitStructure = "64bits"
+    
     AddressOfEntryPoint 
     ImageBase
     SectionAlignment
@@ -48,5 +52,11 @@ def find(fd, NT_Offset):
     SizeOfHeader
     Subsystem
     NumbersOfRvaAndSizes
-    
+    """
+    print("[NT_Header]-Optional Header")
+    print("{}\t\t{}\t\t{} ".format("offset","value","description"))
+    print("============================================")
+    print("{:x}".format(Magic))
+    print(BitStructure)
+    print("{:08x}\t{:04x}\t{}".format(NT_Offset+4, Magic, "Magic"))
     #find location of IMAGE_DATA_DIRECTORY
